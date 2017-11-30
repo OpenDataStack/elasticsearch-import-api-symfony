d863a7a31779af127094cad8c4eae3a0df44bd89
Achraf Noomane <noomane.achraf@gmail.com>
2017-11-30 22:39:45 +0100

    listing import configurations implemented

--- src/OpenDataStackBundle/Controller/DefaultController.php
+++ src/OpenDataStackBundle/Controller/DefaultController.php
@@ -8,54 +8,52 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Enqueue\Fs\FsConnectionFactory;
 use Symfony\Component\Filesystem\Exception\IOException;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
     /**
      * @Route("/test")
      */
-     public function indexAction()
-     {
-       // TODO Shift to services
-       $connectionFactory = new FsConnectionFactory('/tmp/enqueue');
-       $context = $connectionFactory->createContext();
+    public function indexAction() {
+        // TODO Shift to services
+        $connectionFactory = new FsConnectionFactory('/tmp/enqueue');
+        $context = $connectionFactory->createContext();
 
-       $data = [
-         'importer' => 'opendatastack/csv',
-         'uri' => 'https://datos.colombiacompra.gov.co/csvdata/2013/20136.csv'
-       ];
-       $queue = $context->createQueue('importQueue');
-       $context->createProducer()->send(
-         $queue,
-         $context->createMessage(json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES))
-       );
+        $data = [
+            'importer' => 'opendatastack/csv',
+            'uri' => 'https://datos.colombiacompra.gov.co/csvdata/2013/20136.csv'
+        ];
+        $queue = $context->createQueue('importQueue');
+        $context->createProducer()->send(
+            $queue,
+            $context->createMessage(json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES))
+        );
 
-       // Example of passing environment variables from docker
-       $elastic_server = $this->getParameter('elastic_server_host');
-       return $this->render('OpenDataStackBundle:Default:index.html.twig',
-         [
-           'message' => 'Hello there',
-           'elastic_server' => $elastic_server,
-         ]
-       );
-     }
+        // Example of passing environment variables from docker
+        $elastic_server = $this->getParameter('elastic_server_host');
+        return $this->render('OpenDataStackBundle:Default:index.html.twig',
+            [
+                'message' => 'Hello there',
+                'elastic_server' => $elastic_server,
+            ]
+        );
+    }
 
 
-     /**
+    /**
      * @Route("/ping")
      * @Method("POST")
      */
-     public function pingAction(Request $request)
-     {
+    public function pingAction(Request $request) {
 
-       $data = $request->request->get('data', 'response ok : send a message in a data key');
+        $data = $request->request->get('data', 'response ok : send a message in a data key');
 
-       $response = new JsonResponse($data, 200);
+        $response = new JsonResponse($data, 200);
 
-       return $response;
-     }
+        return $response;
+    }
 
 
     /**
@@ -75,64 +73,98 @@ class DefaultController extends Controller
      *   },
      * )
      */
-    public function addImportConfigurationAction(Request $request)
-    {
-      $payloadJson = $request->getContent();
+    public function addImportConfigurationAction(Request $request) {
+        $payloadJson = $request->getContent();
 
-      if (!$payloadJson) {
+        if (!$payloadJson) {
+            $response = new JsonResponse(
+                array(
+                    "log" => array(
+                        "status" => "fail",
+                        "message" => "empty config parameters"
+                    )
+                ),
+                400);
+            return $response;
+        }
+
+        $payload = json_decode($payloadJson);
+        if (json_last_error() != JSON_ERROR_NONE) {
+            $response = new JsonResponse(
+                array(
+                    "log" => array(
+                        "status" => "fail",
+                        "message" => json_last_error_msg()
+                    )
+                ),
+                400);
+            return $response;
+        }
+
+        if (!property_exists($payload, "id") || !property_exists($payload, "type") || !property_exists($payload, "config")) {
+            $response = new JsonResponse(
+                array(
+                    "log" => array(
+                        "status" => "fail",
+                        "message" => "Missing keys"
+                    )
+                ),
+                400);
+            return $response;
+        }
+
+        $udid = $payload->id;
+        $fs = $this->container->get('filesystem');
+
+        if ($fs->exists("/tmp/configurations/{$udid}")) {
+            $response = new JsonResponse(
+                array(
+                    "log" => array(
+                        "status" => "fail",
+                        "message" => "Import configuration exist already"
+                    )
+                ),
+                400);
+            return $response;
+        }
+
+        // Persist import-configuration in the filesystem
+        try {
+            $fs->mkdir("/tmp/configurations/{$udid}");
+        } catch (IOException $exception) {
+            $response = new JsonResponse(
+                array(
+                    "log" => array(
+                        "status" => "fail",
+                        "message" => "Folder creation error"
+                    )
+                ),
+                400);
+            return $response;
+        }
+
+        $date = new \DateTime('now');
+        $timestamp = $date->format('Y-m-d H:i:s');
+        $log = array(
+            "status" => "new",
+            "message" => "{$udid} created at {$timestamp}",
+            "created_at" => $timestamp
+        );
+
+        $logJson = json_encode($log);
+        file_put_contents("/tmp/configurations/{$udid}/log.json", $logJson);
+        file_put_contents("/tmp/configurations/{$udid}/config.json", $payloadJson);
+
         $response = new JsonResponse(
-          array("log" => array("status" => "fail", "message" => "empty config parameters")),
-          400);
+            array(
+                "log" => array(
+                    "status" => "success",
+                    "message" => $log['message'],
+                    "flag" => $log['status']
+                )
+            ),
+            200);
         return $response;
-      }
-
-      $payload = json_decode($payloadJson);
-      if (json_last_error() != JSON_ERROR_NONE) {
-        $response = new JsonResponse(
-          array("log" => array("status" => "fail", "message" => json_last_error_msg())),
-          400);
-        return $response;
-      }
-
-      if (!property_exists($payload, "id") || !property_exists($payload, "type") || !property_exists($payload, "config")) {
-        $response = new JsonResponse(
-          array("log" => array("status" => "fail", "message" => "Missing keys")),
-          400);
-        return $response;
-      }
-
-      $udid = $payload->id;
-      $fs = $this->container->get('filesystem');
-
-      if ($fs->exists("/tmp/{$udid}")) {
-        $response = new JsonResponse(
-          array("log" => array("status" => "fail", "message" => "Import configuration exist already")),
-          400);
-        return $response;
-      }
-
-      // Persist import-configuration in the filesystem
-      try {
-        $fs->mkdir("/tmp/{$udid}");
-      } catch (IOException $exception) {
-        $response = new JsonResponse(
-          array("log" => array("status" => "fail", "message" => "Folder creation error")),
-          400);
-        return $response;
-      }
-
-      $date = new \DateTime('now');
-      $timestamp = $date->format('Y-m-d H:i:s');
-      $log = array("status" => "new", "message" => "{$udid} created at {$timestamp}", "created_at" => $timestamp);
-
-      $logJson = json_encode($log);
-      file_put_contents("/tmp/{$udid}/log.json", $logJson);
-      file_put_contents("/tmp/{$udid}/config.json", $payloadJson);
-
-      $response = new JsonResponse(
-        array("log" => array("status" => "success", "message" => $log['message'], "flag" => $log['status'])),
-        200);
-      return $response;
 
     }
 
@@ -162,67 +194,108 @@ class DefaultController extends Controller
      */
     public function statusConfigurationAction($udid) {
 
-      if (!$udid){
+        if (!$udid) {
+            $response = new JsonResponse(
+                array(
+                    "log" => array(
+                        "status" => "fail",
+                        "message" => "udid parameters required"
+                    )
+                ),
+                400);
+            return $response;
+        }
+
+        if (!file_exists("/tmp/configurations/{$udid}")) {
+            $response = new JsonResponse(
+                array(
+                    "log" => array(
+                        "status" => "fail",
+                        "message" => "no configuration with the udid: {$udid}"
+                    )
+                ),
+                404);
+            return $response;
+        }
+
+        $logJson = file_get_contents("/tmp/configurations/{$udid}/log.json");
+        $log = json_decode($logJson);
+
         $response = new JsonResponse(
-          array("log" => array("status" => "fail", "message" => "udid parameters required")),
-          400);
+            array(
+                "log" => array(
+                    "status" => "success",
+                    "message" => $log->message,
+                    "flag" => $log->status
+                )
+            ),
+            200);
         return $response;
-      }
-
-      if (!file_exists("/tmp/{$udid}")) {
-        $response = new JsonResponse(
-          array("log" => array("status" => "fail", "message" => "no configuration with the udid: {$udid}")),
-          404);
-        return $response;
-      }
-
-      $logJson = file_get_contents("/tmp/{$udid}/log.json");
-      $log = json_decode($logJson);
-
-      $response = new JsonResponse(
-        array("log" => array("status" => "success", "message" => $log->message, "flag" => $log->status)),
-        200);
-      return $response;
 
     }
 
-  /**
-   * @Route("/import-configurations")
-   * @Method("GET")
-   * @ApiDoc(
-   *   description="Returns a list of Import configurations",
-   *   tags={"in-development"},
-   *   method="GET",
-   *   section="Import Configurations",
-   *   statusCodes={
-   *       200="success",
-   *       400="error",
-   *       404="not found",
-   *   }
-   *
-   * )
-   */
-  public function statusConfigurationsListAction() {
+    /**
+     * @Route("/import-configurations")
+     * @Method("GET")
+     * @ApiDoc(
+     *   description="Returns a list of Import configurations",
+     *   tags={"in-development"},
+     *   method="GET",
+     *   section="Import Configurations",
+     *   statusCodes={
+     *       200="success",
+     *       400="error",
+     *       404="not found",
+     *   }
+     *
+     * )
+     */
+    public function statusConfigurationsListAction() {
+
+        $finder = new Finder();
+        $folders = $finder->directories()->in("/tmp/configurations");
+
+        $listImportConfigurations = NULL;
+        foreach ($folders as $folder) {
+            $listImportConfigurations[] = basename($folder);
+        }
+
+        if (!$listImportConfigurations) {
+            $response = new JsonResponse(
+                array(
+                    "ids" => array()
+                ),
+                404);
+
+            return $response;
+        }
+
+        $response = new JsonResponse(
+            array(
+                "ids" => $listImportConfigurations
+            ),
+            200);
+
+        return $response;
 
     }
 
     /**
      * @Route("/debug")
      */
-      public function debugAction() {
+    public function debugAction() {
 
-        $message = "folder /tmp/photos does not exist";
+        $finder = new Finder();
+        $folders = $finder->directories()->in("/tmp/configurations");
 
-        $fs = $this->container->get('filesystem');
-        //$fs->mkdir("/tmp/photos");
-
-        if ($fs->exists("/tmp/photos")) {
-          $message = "folder /tmp/photos exist";
+        $b = NULL;
+        foreach ($folders as $f) {
+            $b[] = basename($f);
         }
 
         return $this->render('OpenDataStackBundle:Default:debug.html.twig',
-          ['message' => $message]
+            ['message' => $b]
         );
-      }
-         
+    }
+
 }
