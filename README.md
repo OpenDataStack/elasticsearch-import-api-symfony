af76dba32c3a90f32f13b03a182e79324a234d5e
Achraf Noomane <noomane.achraf@gmail.com>
2017-12-18 10:49:25 +0100

    CSV import & Elasticsearch document pushed

--- README.md
+++ README.md
@@ -1,27 +1,34 @@
 #Summary
 
-
-A Symfony application designed to handle csv import and index data to elasticsearch. The application
-have a REST Api endpoint and a message queue based on filesystem
+A Symfony application designed to handle dataset resources import and index to elasticsearch. The application
+have a REST Api endpoint and a message queue based on filesystem. 
 
 ##How it works
 
-The purpose of this app is to be an importer for datasets resources. for each dataset, a new folder is created holding the data schema and the status log. When a request to import is made to the API, a resource will be queued to be downloaded, parsed and indexed to elasticsearch
+For each dataset, a new folder is created holding the data schema and the status log. 
+When a request to import is made to the API, a resource uri is add to the queue in order to be fetched, parsed and indexed to elasticsearch
+
 
 ##Usage
 The homepage of the project shows the API documentation made by Nelmio bundle. 
 You can see the Rest api methods and a useful sandbox foreach method to test it
 
+to run the project using Php internal server, run the following command:
+
+```bash
+> php bin/console server:start
+```
+and open the url : [localhost:8000](http://localhost:8000)
 #### Endpoints
 
 - **Model**
-  - POST import-configuration
-  - GET import-configuration/$id
-  - GET import-configurations
-  - DELETE import-configuration/$id
+  - [POST] import-configuration
+  - [GET] import-configuration/$id
+  - [GET] import-configurations
+  - [DELETE] import-configuration/$id
 
 - **Import Processing**
-  - POST import-request
+  - [POST] import-request
 
 #### Queue
 To launch the queue listener for incoming request to import , run the following symfony command from the root of the project :
--- src/OpenDataStackBundle/Command/ImportCommand.php
+++ src/OpenDataStackBundle/Command/ImportCommand.php
@@ -3,7 +3,6 @@
 namespace OpenDataStackBundle\Command;
 
 use Elasticsearch\ClientBuilder;
-use Elasticsearch\Common\Exceptions\BadRequest400Exception;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -27,181 +26,91 @@ class ImportCommand extends ContainerAwareCommand {
             ->setDescription('Process the import file queue');
     }
 
+    /**
+     * Handle requests to import dkan resources and prepare them for indexing
+     *
+     */
     protected function execute(InputInterface $input, OutputInterface $output) {
         $output->writeln("Listening for the Queue: --importQueue--");
 
-        // TODO: Shift to services
+        // Initialise a Queue consumer
         $connectionFactory = new FsConnectionFactory('/tmp/enqueue');
         $context = $connectionFactory->createContext();
         $importQueue = $context->createQueue('importQueue');
         $queueConsumer = new QueueConsumer($context);
 
-        $logger = ClientBuilder::defaultLogger('/tmp/importer.log');
-
+        // Initialise Elasticsearch PHP Client
         $client = ClientBuilder::create()
             ->setHosts(['http://elasticsearch:9200'])
-            ->setLogger($logger)
+            ->setLogger(ClientBuilder::defaultLogger('/tmp/importer.log'))
             ->setSSLVerification(false)
             ->build();
 
 
+        // anonymous block function to handle incoming requests
         $queueConsumer->bind('importQueue', function (PsrMessage $message) use (&$output, $client) {
-            // update log file
+
             $output->writeln("Processing Job Import");
 
+            // Parse payload from the REST call
             $data = json_decode($message->getBody(), TRUE);
+            $udid = $data['udid'];
+            $resourceId = $data['id'];
             $output->writeln("Import Type: " . $data['importer']);
             $output->writeln("URI: " . $data['uri']);
 
-
-            $uniqueFileName = vsprintf("resource_%s.csv", uniqid());
-
             // 1. Download CSV Resource
+            $uniqueFileName = vsprintf("resource_%s.csv", uniqid());
             $filePath = "/tmp/configurations/" . $data['udid'] . "/" . $uniqueFileName;
             $uri = $data['uri'];
-            $this->downloadFile($uri, $filePath);
 
-            $udid = $data['udid'];
-
+            file_put_contents($filePath, fopen($uri, 'r'));
             $output->writeln("Resource for dataset({$udid}) downloaded successfully");
 
             // 2. Parse CSV Resource
-
             $csv = Reader::createFromPath($filePath, 'r');
             $csv->setHeaderOffset(0);
 
             $header = $csv->getHeader(); //returns the CSV header record
             $records = $csv->getRecords(); //returns all the CSV records as an Iterator object
 
+            // 3. Clear & Recreate index
+            $indexName = 'dkan-'.$udid.'-'.$resourceId;
+            if ($client->indices()->exists(['index' => $indexName])) {
+                $client->indices()->delete(['index' => $indexName]);
+            }
+            $response = $client->indices()->create(['index' => $indexName]);
 
-            // 3. Create/Update Index
-
-            // X. Create/Update Index TEMPLATE (goes in addConfiguration)
-/*
-
-            $configJson = file_get_contents("/tmp/configurations/{$udid}/config.json");
-            $datasetConfig = json_decode($configJson);
-
-            $mappings = $datasetConfig->config->mappings;
-            $elasticIndex = "dkan-".$udid;
-
-
-            $params = [
-                'index' => $elasticIndex,
-                'body' => [
-                    'settings' => [
-                        'number_of_shards' => 3,
-                        'number_of_replicas' => 2
-                    ],
-                    'mappings' => $mappings
-                ]
-            ];
-
-            $response = $client->indices()->create($params);
-*/
-
-            print_r("---------------------");
-            die();
-            $params = ['body' => []];
-
-
-            foreach ($records as $key => $rowFields ) {
-
-            // $value : [ocid] => ocds-k50g02-13-10-153724
-
-            //print_r($key);
-            //print_r($value);
-            //sprintf("key:%s || value:%s", $key, $value);
-            //sprintf("header:%s", $header[$key]);
-
+            // 4. Batch index 1000 records at a time
+            foreach ($records as $key => $rowFields) {
                 $params['body'][] = [
                     'index' => [
-                        '_index' => $elasticIndex,
-                        '_type' => "dkantype"
+                        '_index' => $indexName,
+                        '_type' => 'contract'
                     ]
                 ];
-
-                // array data
                 $params['body'][] = $rowFields;
 
                 // Every 1000 documents stop and send the bulk request
-                if ($key % 10 == 0) {
-                    $responses = $client->bulk($params);
+                if ($key % 1000 == 0) {
+                    $response = $client->bulk($params);
+                    print_r($response);
 
-                    $output->writeln(" -- bulk response -- ");
-                    print_r($responses);
-                    $output->writeln(" -- end bulk response -- ");
-
-                    // erase the old bulk request
                     $params = ['body' => []];
-
-                    // unset the bulk response when you are done to save memory
-                    unset($responses);
                 }
             }
 
-            $output->writeln(" -- end foreach -- ");
-
-
-            // Send the last batch if it exists
+            // Send the last batch
             if (!empty($params['body'])) {
-                $responses = $client->bulk($params);
-
-                $output->writeln(" -- final bulk response -- ");
-                print_r($responses);
-                $output->writeln(" -- final bulk response -- ");
-
+                $response = $client->bulk($params);
+                print_r($response);
             }
 
-
-
-            //print_r($header);
-            //var_dump(iterator_count($records));
-
-            // test link
-            // https://cdn.rawgit.com/achoura/elkd/aa0074ac/model_slug.csv
-            // https://cdn.rawgit.com/achoura/elkd/master/model.csv
-
-            // Original
-            // https://datos.colombiacompra.gov.co/csvdata/2013/20135.csv
-
-
-            $output->writeln("*****************************");
-            $output->writeln("File parsed");
-
-
-            // update log file
-            // catch any exceptions and update log file with error
             return PsrProcessor::ACK;
+
         });
 
         $queueConsumer->consume();
     }
 
-    // helpers
-    private function downloadFile($url, $path) {
-        $newfname = $path;
-
-        $opts = array(
-            "ssl" => array(
-                "verify_peer" => FALSE,
-                "verify_peer_name" => FALSE,
-            ),
-        );
-        $file = fopen($url, 'rb', FALSE, stream_context_create($opts));
-        if ($file) {
-            $newf = fopen($newfname, 'wb');
-            if ($newf) {
-                while (!feof($file)) {
-                    fwrite($newf, fread($file, 1024 * 8), 1024 * 8);
-                }
-            }
-        }
-        if ($file) {
-            fclose($file);
-        }
-        if ($newf) {
-            fclose($newf);
-        }
-    }
 }
